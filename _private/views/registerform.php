5ce0d6440b964d8b2c12c2ccf8af16238d0f5f74
Jelani Alexis <31781@ma-web.nl>
2021-10-06 11:41:53 +0200

    removed unnecessary elements & bootstrap

--- _private/views/registerform.php
+++ _private/views/registerform.php
@@ -1,24 +1,27 @@
 <?php $this->layout('layouts::website'); ?>
 
-<h1>Inschrijven</h1>
+<h1>Aanmelden</h1>
 
 <p>Schrijf je hier in om toegang te krijgen tot Transformers.community!</p>
 
 <form action="<?php echo url('register.handle')?>" method="POST">
-    <div class="form-group">
+    <div>
         <label for="email">E-mail</label>
         <input type="email" name="email" value="<?php echo input('email')?>" class="form-control" id="email" aria-describedby="emailHelp">
-        <small id="emailHelp" class="form-text text-muted">Je email zal met niemand gedeeld worden, het wordt exclusief gebruikt voor inloggen en evt. opname van contact.</small>
         <?php if ( isset( $errors['email'] ) ):?>
             <?php echo $errors['email']?>
         <?php endif;?>
-    </div>
-    <div class="form-group">
+    </div><br>
+
+    <div>
         <label for="wachtwoord">Wachtwoord</label>
         <input type="password" name="wachtwoord" class="form-control" id="wachtwoord">
         <?php if ( isset( $errors['wachtwoord'] ) ):?>
             <?php echo $errors['wachtwoord']?>
         <?php endif;?>
-    </div>
-    <button type="submit" class="btn btn-primary">Aanmelden</button>
+    </div><br>
+    <hr>
+    <button type="submit">Aanmelden</button><br>
+
+    <small id="emailHelp">Door u aan te melden geeft u toestemming dat wij contact met u op kunnen nemen. Wij zullen uw persoonlijke informatie nergens anders voor gebruiken.</small>
 </form>
